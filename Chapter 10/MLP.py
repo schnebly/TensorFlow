96a74f5071bdbf0f4bda71360576dbbbcf9f6b16
James Schnebly <jameschnebly@gmail.com>
2018-06-26 21:16:14 -0700

    Add files via upload

---
+++ Chapter 10/MLP.py
@@ -0,0 +1,150 @@
+'''
+Author = James Schnebly
+Date = 6/26/18
+Project = chapter 10 exercise 9
+
+Tittle = Multi-Layer Perceptron (MLP)
+Task = Classify MNIST @ >98%
+'''
+import tensorflow as tf
+import numpy as np
+
+
+#reset default graph
+tf.reset_default_graph
+
+# specify number of attributes and neurons in each layer
+n_inputs = 28*28
+n_hidden1 = 300
+n_hidden2 = 100
+n_output = 10
+
+# preprocessing
+(X_train, y_train), (X_test, y_test) = tf.keras.datasets.mnist.load_data()
+X_train = X_train.astype(np.float32).reshape(-1, 28*28) / 255.0
+X_test = X_test.astype(np.float32).reshape(-1, 28*28) / 255.0
+y_train = y_train.astype(np.int32)
+y_test = y_test.astype(np.int32)
+X_valid, X_train = X_train[:5000], X_train[5000:]
+y_valid, y_train = y_train[:5000], y_train[5000:]
+
+# create placeholders
+X = tf.placeholder(tf.float32, shape=(None, n_inputs), name='X')
+y = tf.placeholder(tf.int32, shape=(None), name='y')
+
+# create the layer architecture
+with tf.name_scope("dnn"):
+    hidden1 = tf.layers.dense(X, n_hidden1, name="hidden1", activation=tf.nn.relu)
+    hidden2 = tf.layers.dense(hidden1, n_hidden2, name="hidden2", activation=tf.nn.relu)
+    logits = tf.layers.dense(hidden2, n_output, name="outputs")
+    
+
+# specify the loss function
+with tf.name_scope("loss"):
+    x_entropy = tf.nn.sparse_softmax_cross_entropy_with_logits(labels=y, logits=logits)
+    loss = tf.reduce_mean(x_entropy, name="loss")
+    loss_summary = tf.summary.scalar("log_loss", loss)
+
+learning_rate = 0.01
+
+# specify train
+with tf.name_scope("train"):
+    optimizer = tf.train.GradientDescentOptimizer(learning_rate)
+    training_op = optimizer.minimize(loss)
+    
+#specify eval
+with tf.name_scope("eval"):
+    correct = tf.nn.in_top_k(logits, y, 1)
+    accuracy = tf.reduce_mean(tf.cast(correct, tf.float32))
+    accuracy_summary = tf.summary.scalar("accuracy", accuracy)
+    
+# initialize placeholders and saver
+init = tf.global_variables_initializer()
+saver = tf.train.Saver()
+
+##### Tensorboard stuff // LOGS ######
+from datetime import datetime
+
+def log_dir(prefix=""):
+    now = datetime.utcnow().strftime("%Y%m%d%H%M%S")
+    root_logdir = "tf_logs"
+    if prefix:
+        prefix += "-"
+    name = prefix + "run-" + now
+    return "{}/{}/".format(root_logdir, name)
+
+def shuffle_batch(X, y, batch_size):
+    rnd_idx = np.random.permutation(len(X))
+    n_batches = len(X) // batch_size
+    for batch_idx in np.array_split(rnd_idx, n_batches):
+        X_batch, y_batch = X[batch_idx], y[batch_idx]
+        yield X_batch, y_batch
+
+logdir = log_dir("mnist_dnn")
+
+# file writer
+file_writer = tf.summary.FileWriter(logdir, tf.get_default_graph())
+
+# prepare to run
+m,n = X_train.shape
+n_epochs = 10001
+batch_size = 50
+n_batches = int(np.ceil(m / batch_size))
+
+checkpoint_path = "tmp/my_deep_mnist_model.ckpt"
+checkpoint_epoch_path = checkpoint_path + ".epoch"
+final_model_path = "./my_deep_mnist_model"
+
+best_loss = np.infty
+epochs_without_progress = 0
+max_epochs_without_progress = 50
+
+import os
+# start
+with tf.Session() as sess:
+    # if epoch checkpoint exists, restore and load the epoch
+    if os.path.isfile(checkpoint_epoch_path):
+        with open(checkpoint_epoch_path, "rb") as f:
+            start_epoch = int(f.read())
+        print("Training was interrupted. Continue at epoch", start_epoch)
+        saver.restore(sess, checkpoint_path)
+    else:
+        start_epoch = 0
+        sess.run(init)
+        
+    for epoch in range(start_epoch, n_epochs):
+        for X_batch, y_batch in shuffle_batch(X_train, y_train, batch_size):
+            sess.run(training_op, feed_dict={X: X_batch, y: y_batch})
+        accuracy_val, loss_val, accuracy_summary_str, loss_summary_str = sess.run([accuracy, loss, accuracy_summary, loss_summary], feed_dict={X: X_valid, y: y_valid})
+        file_writer.add_summary(accuracy_summary_str, epoch)
+        file_writer.add_summary(loss_summary_str, epoch)
+        
+        if epoch % 5 == 0:
+            print("Epoch:", epoch, "\tValidation accuracy: {:.3f}%".format(accuracy_val * 100), "\tLoss: {:.5f}".format(loss_val))
+            saver.save(sess, checkpoint_path)
+            with open(checkpoint_epoch_path, "wb") as f:
+                f.write(b"%d" % (epoch + 1))
+            if loss_val < best_loss:
+                saver.save(sess, final_model_path)
+                best_loss = loss_val
+            else:
+                epochs_without_progress += 5
+                if epochs_without_progress > max_epochs_without_progress:
+                    print("Early Stopping")
+                    break
+                
+os.remove(checkpoint_epoch_path)
+
+with tf.Session() as sess:
+    saver.restore(sess, final_model_path)
+    accuracy_val = accuracy.eval(feed_dict={X: X_test, y: y_test})
+    
+print("Final Accuracy:", accuracy_val )
+
+
+        
+        
+
+
+
+
